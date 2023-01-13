d4f0889eb13d2a9386e232b05e09a6732dd48987
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-01-13 06:59:04 +0100

    update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,7 +2,7 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  aws_user_pools_web_client_id: "63r3tb0hlr658ccc237h4421im", // CognitoClientID
+  aws_user_pools_web_client_id: "5at47g7g3leiadv16md5uhmnj8", // CognitoClientID
   api_base_url: "None", // TodoFunctionApi
   cognito_hosted_domain: "serverless-todo.auth.eu-central-1.amazoncognito.com", // CognitoDomainName
   redirect_url: "https://master.d2h5jfasl3mgxo.amplifyapp.com", // AmplifyURL
