5e96b57ff6f0a05a64f19c8557115909e0341ad4
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2023-01-17 07:14:24 +0100

    use correct redirect url

--- www/src/config.ts
+++ www/src/config.ts
@@ -6,8 +6,7 @@ const config = {
   api_base_url:
     "https://wykyzwln7h.execute-api.eu-central-1.amazonaws.com/serverless_lambda_stage", // TodoFunctionApi
   cognito_hosted_domain: "serverless-todo.auth.eu-central-1.amazoncognito.com", // CognitoDomainName
-  // redirect_url: "https://master.d1pka7or1wopsp.amplifyapp.com", // AmplifyURL
-  redirect_url: "http://localhost:8080", // AmplifyURL
+  redirect_url: "https://master.d1pka7or1wopsp.amplifyapp.com", // AmplifyURL
 } as const;
 
 export default config;
